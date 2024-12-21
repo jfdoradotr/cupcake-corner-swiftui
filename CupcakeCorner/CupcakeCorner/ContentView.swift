879a24eb7198e8d3ace773ae4239cf52cf807dd9
Juan Dorado <jfdoradotr@icloud.com>
2024-12-21 16:48:46 -0600

    Create the basic structure for the model to decode of the API

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -4,6 +4,16 @@
 
 import SwiftUI
 
+struct Response: Codable {
+  var results: [Result]
+}
+
+struct Result: Codable {
+  var trackId: Int
+  var trackName: String
+  var collectionName: String
+}
+
 struct ContentView: View {
   var body: some View {
     VStack {
