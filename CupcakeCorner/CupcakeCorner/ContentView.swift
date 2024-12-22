9b904e4a56e623e7ef3b942b19a4850215578e94
Juan Dorado <jfdoradotr@icloud.com>
2024-12-22 13:33:51 -0600

    Add the navigation to the AddressView

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -47,6 +47,12 @@ struct ContentView: View {
             Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
           }
         }
+
+        Section {
+          NavigationLink("Delivery details") {
+            AddressView(order: order)
+          }
+        }
       }
       .navigationTitle("Cupcake Corner")
     }
