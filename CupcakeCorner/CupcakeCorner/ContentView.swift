e0652d96c02f985a6efaa8441bb3fdfee2c9a21d
Juan Dorado <jfdoradotr@icloud.com>
2024-12-21 19:36:31 -0600

    Create a basic form

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -4,59 +4,22 @@
 
 import SwiftUI
 
-struct Response: Codable {
-  var results: [Result]
-}
-
-struct Result: Codable {
-  var trackId: Int
-  var trackName: String
-  var collectionName: String
-}
-
 struct ContentView: View {
-  @State private var results = [Result]()
+  @State private var username = ""
+  @State private var email = ""
 
   var body: some View {
-    VStack {
-      AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-        if let image = phase.image {
-          image
-            .resizable()
-            .scaledToFit()
-        } else if phase.error != nil {
-          Text("There was an error loading the image")
-        } else {
-          ProgressView()
+    Form {
+      Section {
+        TextField("Username", text: $username)
+        TextField("Email", text: $email)
+      }
+
+      Section {
+        Button("Create account") {
+          print("Creating account...")
         }
       }
-      .frame(width: 200, height: 200)
-      List(results, id: \.trackId) { item in
-        VStack(alignment: .leading) {
-          Text(item.trackName)
-            .font(.headline)
-          Text(item.collectionName)
-        }
-      }
-    }
-    .task {
-      await loadData()
-    }
-  }
-
-  private func loadData() async {
-    guard let url = URL(string: "https://itunes.apple.com/search?term=metallica&entity=song") else {
-      print("Invalid URL")
-      return
-    }
-
-    do {
-      let (data, _) = try await URLSession.shared.data(from: url)
-      if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-        results = decodedResponse.results
-      }
-    } catch {
-      print("Invalid data")
     }
   }
 }
