0da3f7876c65201fd73e107b72851e5492f61246
Juan Dorado <jfdoradotr@icloud.com>
2024-12-22 13:36:55 -0600

    Create the basic CheckoutView view

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,17 @@
+//
+// Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+  var order: Order
+
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  CheckoutView(order: Order())
+}
