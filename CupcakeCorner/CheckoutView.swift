fef84ff597d61de08b5b9bd69f567db7e65dfbec
Juan Dorado <jfdoradotr@icloud.com>
2024-12-22 15:02:46 -0600

    Create the checkout view

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -8,10 +8,28 @@ struct CheckoutView: View {
   var order: Order
 
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ScrollView {
+      VStack {
+        AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+          image
+            .resizable()
+            .scaledToFit()
+        } placeholder: {
+          ProgressView()
+        }
+        Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+          .font(.title)
+        Button("Place Order", action: { })
+          .padding()
+      }
+    }
+    .navigationTitle("Check out")
+    .navigationBarTitleDisplayMode(.inline)
   }
 }
 
 #Preview {
-  CheckoutView(order: Order())
+  NavigationStack {
+    CheckoutView(order: Order())
+  }
 }
