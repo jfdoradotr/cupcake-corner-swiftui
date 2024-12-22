768ff06644d5a258a5e6ede1994ad809795cdc0b
Juan Dorado <jfdoradotr@icloud.com>
2024-12-22 14:55:20 -0600

    Validate the address otherwise disable the section form

--- CupcakeCorner/AddressView.swift
+++ CupcakeCorner/AddressView.swift
@@ -21,6 +21,7 @@ struct AddressView: View {
           CheckoutView(order: order)
         }
       }
+      .disabled(order.hasValidAddress == false)
     }
     .navigationTitle("Delivery details")
     .navigationBarTitleDisplayMode(.inline)
